64a1b7cc1eb3248baeb0ac10cdd950d319420f84
ajstanley <alanjarlathstanley@gmail.com>
2016-05-19 12:48:07 -0300

    Fixed inconsistent expansion behavior

--- js/agile_newspaper.js
+++ js/agile_newspaper.js
@@ -2,17 +2,20 @@
 (function ($) {
     Drupal.behaviors.agile_newspaper = {
         'attach': function (context) {
-            $('.vertical-tab-button').hide();
-            $('.vertical-tabs-panes').first().hide(); // Hide the 1904 tab
-            $('#agile_year_select').change(function(){
+            $(".month").addClass('collapsed');
+            var switcher = function () {
                 var selected = $('#agile_year_select option:selected').text();
                 var selector = '.fieldset-legend:contains(' + selected + ')';
-                var active = $(selector).closest('fieldset');
-                console.log(active)
+                var active = $(selector).closest('.form-wrapper').wrap('<fieldset class="form-wrapper"></fieldset>');
                 $('#current_year').html(active.html());
-                
-            })
-            
+                $('#current_year').find('a.fieldset-title').click(function () {
+                    Drupal.toggleFieldset($(this).closest('fieldset.collapsible'));
+                });
+            }
+            $('.vertical-tab-button').hide();
+            $('.vertical-tabs-panes').first().hide();
+            switcher();
+            $('#agile_year_select').change(switcher)
         }
     };
 })(jQuery);
