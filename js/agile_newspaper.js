02b39435bc1b1a05d6d54803cd09f18e42f2a9e5
ajstanley <alanjarlathstanley@gmail.com>
2016-05-27 10:57:45 -0300

    lazy load

--- js/agile_newspaper.js
+++ js/agile_newspaper.js
@@ -1,5 +1,11 @@
 
 (function ($) {
+    Drupal.agile_newspaper_expand = function () {
+        Drupal.toggleFieldset($('#current_year').find('fieldset.collapsible'));
+        $('#current_year').find('.agile_newspaper_thumb').each(function(){
+            $(this).attr('src', $(this).attr('data'));
+        });
+    };
     Drupal.behaviors.agile_newspaper = {
         'attach': function (context) {
             $(".month").addClass('collapsed');
@@ -10,6 +16,10 @@
                 $('#current_year').html(active.html());
                 $('#current_year').find('a.fieldset-title').click(function () {
                     Drupal.toggleFieldset($(this).closest('fieldset.collapsible'));
+                    var $month = $(this).closest('fieldset.month');
+                    $month.find('img').each(function () {
+                        $(this).attr('src', $(this).attr('data'));
+                    });
                 });
             }
             $('.vertical-tab-button').hide();
